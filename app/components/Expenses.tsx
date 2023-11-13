e5efb3b006a0c2cb2148e51d4ff9612afa3fd9e2
asubaruwrxsti <arlind12344@gmail.com>
2023-11-13 19:48:14 +0100

    made slight changes to expenses component

--- app/components/Expenses.tsx
+++ app/components/Expenses.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/prisma/db";
+import Link from "next/link";
 
 export default async function Expenses() {
 	return (
@@ -10,9 +11,18 @@ export default async function Expenses() {
 						return (
 							<div className={'card mt-1 ml-1 col-lg-5'} key={expense.id}>
 								<div className={'card-body'}>
-									<h6 className={'card-title'}>{expense.name}</h6>
+									<Link href={`/expenses/${expense.id}`} className={'card-title'}>
+										<h6> {expense.name} </h6>
+									</Link>
 									<p className={'card-text'}>{expense.description}</p>
 									<p className={'card-text'}>{expense.amount}</p>
+								</div>
+								<div className={'card-footer'}>
+									<small className={'text-muted'}>Created at: {new Date(expense.createdAt).toLocaleString()}</small>
+									<div className={'float-right'}>
+										<button className={'btn btn-sm btn-outline-primary mr-1'}>Edit</button>
+										<button className={'btn btn-sm btn-outline-danger'}>Delete</button>
+									</div>
 								</div>
 							</div>
 						)
