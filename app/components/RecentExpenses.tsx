663734e5277801f9f70ead2259d8377afaa2391b
Arlind <arlind12344@gmail.com>
2023-12-05 12:55:36 +0100

    Fix formatting in RecentExpenses component

--- app/components/RecentExpenses.tsx
+++ app/components/RecentExpenses.tsx
@@ -2,7 +2,7 @@ import prisma from "@/prisma/db";
 import Link from "next/link";
 
 type ExpenseProps = {
-	take ?: number;
+	take?: number;
 };
 
 export default async function RecentExpenses({ take = 5 }: ExpenseProps) {
@@ -12,30 +12,30 @@ export default async function RecentExpenses({ take = 5 }: ExpenseProps) {
 			Categories: true, // Include the categories information
 		},
 	}) as any[];
-	
+
 	return (
 		<div className="recent-orders">
 			<h2>Recent Expenses</h2>
 			<table>
 				<thead>
-				<tr>
-					<td>Expense</td>
-					<td>Description</td>
-					<td>Amount</td>
-					<td>Category</td>
-					<td>Created At</td>
-				</tr>
+					<tr>
+						<td>Expense</td>
+						<td>Description</td>
+						<td>Amount</td>
+						<td>Category</td>
+						<td>Created At</td>
+					</tr>
 				</thead>
 				<tbody>
-				{recentExpenses.map((expense) => (
-					<tr key={expense.id}>
-						<td>{expense.name}</td>
-						<td>{expense.description}</td>
-						<td>{expense.amount}</td>
-						<td>{expense.Categories.name || 'No category'}</td>
-						<td>{new Date(expense.createdAt).toLocaleString()}</td>
-					</tr>
-				))}
+					{recentExpenses.map((expense) => (
+						<tr key={expense.id}>
+							<td>{expense.name}</td>
+							<td>{expense.description}</td>
+							<td>{expense.amount}</td>
+							<td>{expense.Categories.name || 'No category'}</td>
+							<td>{new Date(expense.createdAt).toLocaleString()}</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 			<Link href={'/expenses'}>View All Expenses</Link>
