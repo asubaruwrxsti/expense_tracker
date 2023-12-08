282d80cd8e42e5cb934c700fb90393c76f7edeaa
asubaruwrxsti <arlind12344@gmail.com>
2023-12-08 14:38:32 +0100

    Add SweetAlert component for loading screen

--- app/todoist/loading.tsx
+++ app/todoist/loading.tsx
@@ -1,12 +1,24 @@
 import Sidebar from "@/app/components/Sidebar";
 import RightSection from "@/app/components/RightSection";
+import SweetAlert from "../components/SweetAlert";
 
 export default function Loading() {
 	return (
 		<div className={'container'}>
 			<Sidebar active={'/expenses'} />
 			<main>
-				<h1>Loading...</h1>
+				<SweetAlert
+					title={'Loading'}
+					text={'Please wait...'}
+					icon={'info'}
+					confirmButtonText={'Ok'}
+					cancelButtonText={'Cancel'}
+					confirmButtonColor={'#d33'}
+					cancelButtonColor={'#3085d6'}
+					showCancelButton={false}
+					showConfirmButton={false}
+					reverseButtons={false}
+				/>
 			</main>
 			<RightSection />
 		</div>
