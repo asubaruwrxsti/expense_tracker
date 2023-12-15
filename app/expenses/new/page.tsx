aacd05330723ae777e6b855f38f9367e11832857
asubaruwrxsti <arlind12344@gmail.com>
2023-12-15 18:06:37 +0100

    Improved SWAL integration

--- app/expenses/new/page.tsx
+++ app/expenses/new/page.tsx
@@ -3,6 +3,8 @@ import Sidebar from "@/app/components/Sidebar";
 import RightSection from "@/app/components/RightSection";
 import prisma from "@/prisma/db";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import Loading from "@/app/components/Loading";
 
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -15,13 +17,15 @@ export default async function NewExpense() {
 	const categories = await prisma.categories.findMany();
 	const selectData = categories.map(category => ({ ...category, id: String(category.id) }));
 	return (
-		<div className={'container'}>
-			<Sidebar active={'/expenses'} />
-			<main>
-				<h1>New Expense Form</h1>
-				<NewExpenseForm selectData={selectData} />
-			</main>
-			<RightSection />
-		</div>
+		<Suspense fallback={<Loading icon="info" />}>
+			<div className={'container'}>
+				<Sidebar active={'/expenses'} />
+				<main>
+					<h1>New Expense Form</h1>
+					<NewExpenseForm selectData={selectData} />
+				</main>
+				<RightSection />
+			</div>
+		</Suspense>
 	)
 }
