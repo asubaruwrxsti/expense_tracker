560f34e162eb5775b778a26a77dc4d6581e16a4b
asubaruwrxsti <arlind12344@gmail.com>
2024-01-03 19:16:14 +0100

    Added GET route for GSHEETS

--- app/api/gsheets/route.tsx
+++ app/api/gsheets/route.tsx
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server';
-import { auth, sheets } from '@/utils/gsheetUtils';
-import { google } from 'googleapis';
+import { getSheetData } from '@/utils/gsheetUtils';
 
 export async function GET(request: Request) {
-    const res = await sheets.spreadsheets.values.get({
-        spreadsheetId: process.env.SPREADSHEET_ID,
-        range: 'A1:E',
-    });
-
-    return NextResponse.json(res.data.values);
+    try {
+        const params = new URLSearchParams(request.url.split('?')[1]);
+        const range = params.get('range') || 'A1:E';
+        const res = await getSheetData(range);
+        return NextResponse.json(res);
+    } catch (error) {
+        return NextResponse.error();
+    }
 }
